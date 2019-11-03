9121ab8ec52b335641a19f92bc864b69e7454264
Jonny Kalambay <jonnyk20@gmail.com>
2019-11-03 22:08:11 +0000

    Detecting and classifying

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hide = el => (el.style.display = "none");
 const show = el => (el.style.display = "initial");
+const formatClassAndScore = (label, score) => `${label}: %${score.toFixed(2) * 100}`
 
+const preview = document.getElementById("preview");
 const image = document.getElementById("image");
 const upload = document.getElementById("upload");
 const loadModelButton = document.getElementById("load");
@@ -9,7 +11,10 @@ const spinner = document.getElementById("spinner");
 const input = document.getElementById("file");
 const preditionsContainer = document.getElementById("predictions");
 
-let model;
+let classificationModel;
+let detectionModel;
+let imageCanvas
+let detections = [];
 
 const handleUpload = event => {
   const { files } = event.target;
@@ -23,36 +28,101 @@ const handleUpload = event => {
   }
 };
 
-const loadModel = async () => {
+const loadModels = async () => {
   show(spinner);
   hide(loadModelButton);
-  model = await mobilenet.load();
+  classificationModel = await mobilenet.load();
+  detectionModel = await cocoSsd.load();
   hide(spinner);
   show(predictButton);
-};
+}
+
+const renderCanvas = () => {
+  const { width: imgW, height: imgH } = image
+  imageCanvas = document.createElement('canvas');
+  imageCanvas.width = imgW
+  imageCanvas.height = imgH
+  const ctx = imageCanvas.getContext('2d')
+  ctx.drawImage(image, 0, 0, imgW, imgH)
+  preview.appendChild(imageCanvas)
+  hide(image)
+}
+
+const renderBoxes = () => {
+  const ctx = imageCanvas.getContext('2d')
+  detections.forEach(({  bbox, class: label, score }) => {
+    const [boxX, boxY, boxW, boxH] = bbox;
+    ctx.lineWidth = 2
+    ctx.fillStyle = "red"
+    ctx.strokeStyle = "red"
+    ctx.rect(boxX, boxY, boxW, boxH)
+  })
+  ctx.stroke()
+}
+
+const classifyBox = async detection => {
+  const { width: imgW, height: imgH } = image;
+  const { height: imageCanvasH, width: imageCanvasW } = imageCanvas;
+  const [boxX, boxY, boxW, boxH] = detection.bbox;
+  const widthScale = imgW / imageCanvasW
+  const heightScale = imgH / imageCanvasH
+
+  const croppedCanvas = document.createElement('canvas')
+  const A = boxX * widthScale// x
+  const B = boxY * heightScale// y
+  const C = imgW // w original
+  const D = imgH // h original
+  const E = 0
+  const F = 0
+  const G = imageCanvasW // w original (scale)
+  const H = imageCanvasH // h original (scale)
+  const ctx = croppedCanvas.getContext("2d")
+  croppedCanvas.height = boxH // cropH
+  croppedCanvas.width = boxW // cropW
+  ctx.drawImage(image, A, B, C, D, E, F, G, H)
+  const classfications = await classificationModel.classify(croppedCanvas)
+  const { className, probability } = classfications[0];
+  const label = formatClassAndScore(className, probability)
+  return { ...detection, label }
+}
+
+const renderClassificatins = detectedAndClassifiedObjects => {
+  const ctx = imageCanvas.getContext('2d')
+  detectedAndClassifiedObjects.forEach(({ label, bbox }) => {
+    const [boxX, boxY] = bbox;
+    ctx.fillText(label, boxX + 20, boxY + 20)
+  })
+}
 
 const makePrecitions = async () => {
   show(spinner);
   hide(predictButton);
-  let predictions = await model.classify(image)
-  if (predictions.length === 0) {
-    predictions.push({ className: 'unknown '})
-  }
+  detections = await detectionModel.detect(image)
+  renderCanvas()
+  renderBoxes()
+  const classifications = await Promise.all(detections.map(classifyBox))
+  renderClassificatins(classifications)
+};
+
+const classifyImage = async () => {
+  show(spinner);
+  hide(predictButton);
+  const results = await classificationModel.classify(image)
+  const formattedresults = results.map(({ className, probability }) => formatClassAndScore(className, probability));
   hide(spinner);
   show(preditionsContainer);
-  renderPrecitions(predictions);
-};
+  renderPrecitions(formattedresults)
+}
 
 const renderPrecitions = predictions => {
   predictions.forEach(prediction => {
-    console.log('PRED', prediction)
     el = document.createElement("li");
-    el.innerText = prediction.className;
+    el.innerText = prediction
     preditionsContainer.appendChild(el);
   });
 };
 
 
 input.onchange = handleUpload;
-loadModelButton.onclick = loadModel;
+loadModelButton.onclick = loadModels;
 predictButton.onclick = makePrecitions;
